4462555dcfd3995ed7f8908c247183fa895c7e14
Tojimatov Abdukarim <walterbriendeveloper@gmail.com>
2024-06-22 20:57:28 +0500

    fixed

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -1,9 +1,10 @@
 const SalesOrder = require("../models/salesOrder");
 const SalesOrderItem = require("../models/salesOrderItem");
+const StockPurchase = require("../models/stockPurchase");
 const Product = require("../models/product");
 const Salary = require("../models/salary");
 const ExpenseSchema = require("../models/expences");
-
+const moment = require("moment");
 const { ErrorHandler } = require("../util/error");
 
 module.exports = {
@@ -18,7 +19,7 @@ module.exports = {
         0
       ).getDate()}-23:59`;
 
-      const totalSales = await SalesOrder.aggregate([
+      const totalDailySales = await SalesOrder.aggregate([
         {
           $match: {
             date: { $gte: startDate, $lte: endDate },
@@ -26,38 +27,148 @@ module.exports = {
         },
         {
           $group: {
-            _id: {
-              year: { $substr: ["$date", 0, 4] },
-              month: { $substr: ["$date", 5, 2] },
-              day: { $substr: ["$date", 8, 2] },
-            },
+            _id: { day: { $substr: ["$date", 8, 2] } },
             total_amount: { $sum: "$total_amount" },
             total_origin_amount: { $sum: "$total_origin_amount" },
             total_income_amount: { $sum: "$total_income_amount" },
             totalDebt: { $sum: "$totalDebt" },
             totalPaid: { $sum: "$totalPaid" },
+            total_onUSD_amount: { $sum: "$total_onUSD_amount" },
+            shippingCost: { $sum: "$shippingCost" },
           },
         },
         {
           $sort: {
-            "_id.year": 1,
-            "_id.month": 1,
             "_id.day": 1,
           },
         },
       ]);
 
-      if (totalSales.length === 0) {
-        return res.status(200).json([]);
-      }
+      const totalDailyExpenses = await ExpenseSchema.aggregate([
+        {
+          $match: {
+            date: { $gte: startDate, $lte: endDate },
+          },
+        },
+        {
+          $group: {
+            _id: { day: { $substr: ["$date", 8, 2] } },
+            total_expenses: { $sum: "$amount" },
+          },
+        },
+        {
+          $sort: {
+            "_id.day": 1,
+          },
+        },
+      ]);
 
-      res.status(200).json(totalSales);
+      const totalDailySalaries = await Salary.aggregate([
+        {
+          $match: {
+            year: parseInt(year, 10),
+            month: parseInt(month, 10),
+          },
+        },
+        {
+          $group: {
+            _id: { day: "$day" },
+            total_salaries: { $sum: "$totalSalary" },
+          },
+        },
+        {
+          $sort: {
+            "_id.day": 1,
+          },
+        },
+      ]);
+
+      const totalDailyStockPurchases = await StockPurchase.aggregate([
+        {
+          $match: {
+            date: { $gte: startDate, $lte: endDate },
+          },
+        },
+        {
+          $group: {
+            _id: { day: { $substr: ["$date", 8, 2] } },
+            total_shippingCost: { $sum: "$shippingCost" },
+            total_costTotalOnUSD: { $sum: "$costTotalOnUSD" },
+            total_costTotal: { $sum: "$costTotal" },
+          },
+        },
+        {
+          $sort: {
+            "_id.day": 1,
+          },
+        },
+      ]);
+
+      const daysInMonth = new Date(year, month, 0).getDate();
+      const days = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+
+      const mergedResults = days.map((day) => {
+        const sale = totalDailySales.find(
+          (s) => s._id.day === String(day).padStart(2, "0")
+        ) || {
+          total_amount: 0,
+          total_origin_amount: 0,
+          total_income_amount: 0,
+          totalDebt: 0,
+          totalPaid: 0,
+          total_onUSD_amount: 0,
+          totalShippingCostOnSale: 0,
+          total_expenses: 0,
+          total_salaries: 0,
+          totalShippingCostOnBuying: 0,
+          totalCostTotalOnUSDOnBuying: 0,
+          totalCostTotalOnUZSOnBuying: 0,
+          total_revenue: 0,
+        };
+        const expense = totalDailyExpenses.find(
+          (e) => e._id.day === String(day).padStart(2, "0")
+        ) || {
+          total_expenses: 0,
+        };
+        const salary = totalDailySalaries.find((s) => s._id.day === day) || {
+          total_salaries: 0,
+        };
+
+        const stockPurchase = totalDailyStockPurchases.find(
+          (sp) => sp._id.day === String(day).padStart(2, "0")
+        ) || {
+          total_shippingCost: 0,
+          total_costTotalOnUSD: 0,
+          total_costTotal: 0,
+        };
+
+        return {
+          day: day,
+          total_amount: sale.total_amount,
+          total_origin_amount: sale.total_origin_amount,
+          total_income_amount: sale.total_income_amount,
+          total_onUSD_amount: sale.total_onUSD_amount,
+          totalDebt: sale.totalDebt,
+          totalPaid: sale.totalPaid,
+          totalShippingCostOnSale: sale.shippingCost,
+          total_expenses: expense.total_expenses,
+          total_salaries: salary.total_salaries,
+          totalShippingCostOnBuying: stockPurchase.total_shippingCost,
+          totalCostTotalOnUSDOnBuying: stockPurchase.total_costTotalOnUSD,
+          totalCostTotalOnUZSOnBuying: stockPurchase.total_costTotal,
+          total_revenue:
+            sale.total_income_amount -
+            expense.total_expenses -
+            salary.total_salaries,
+        };
+      });
+
+      res.status(200).json(mergedResults);
     } catch (err) {
       console.error(err);
       next(new ErrorHandler(400, "Failed to find orders", err.message));
     }
   },
-
   monthsInYear: async function (req, res, next) {
     try {
       const { year } = req.body;
@@ -128,6 +239,27 @@ module.exports = {
         },
       ]);
 
+      const totalMonthlyStockPurchases = await StockPurchase.aggregate([
+        {
+          $match: {
+            date: { $gte: startDate, $lte: endDate },
+          },
+        },
+        {
+          $group: {
+            _id: { month: { $substr: ["$date", 5, 2] } },
+            total_shippingCost: { $sum: "$shippingCost" },
+            total_costTotalOnUSD: { $sum: "$costTotalOnUSD" },
+            total_costTotal: { $sum: "$costTotal" },
+          },
+        },
+        {
+          $sort: {
+            "_id.month": 1,
+          },
+        },
+      ]);
+
       const months = Array.from({ length: 12 }, (_, i) => i + 1);
 
       const mergedResults = months.map((month) => {
@@ -140,7 +272,13 @@ module.exports = {
           totalDebt: 0,
           totalPaid: 0,
           total_onUSD_amount: 0,
-          shippingCost: 0,
+          totalShippingCostOnSale: 0,
+          total_expenses: 0,
+          total_salaries: 0,
+          totalShippingCostOnBuying: 0,
+          totalCostTotalOnUSDOnBuying: 0,
+          totalCostTotalOnUZSOnBuying: 0,
+          total_revenue: 0,
         };
         const expense = totalMonthlyExpenses.find(
           (e) => e._id.month === String(month).padStart(2, "0")
@@ -153,6 +291,14 @@ module.exports = {
           total_salaries: 0,
         };
 
+        const stockPurchase = totalMonthlyStockPurchases.find(
+          (sp) => sp._id.month === String(month).padStart(2, "0")
+        ) || {
+          total_shippingCost: 0,
+          total_costTotalOnUSD: 0,
+          total_costTotal: 0,
+        };
+
         return {
           month: month,
           total_amount: sale.total_amount,
@@ -161,9 +307,12 @@ module.exports = {
           total_onUSD_amount: sale.total_onUSD_amount,
           totalDebt: sale.totalDebt,
           totalPaid: sale.totalPaid,
-          totalShippingCost: sale.shippingCost,
+          totalShippingCostOnSale: sale.shippingCost,
           total_expenses: expense.total_expenses,
           total_salaries: salary.total_salaries,
+          totalShippingCostOnBuying: stockPurchase.total_shippingCost,
+          totalCostTotalOnUSDOnBuying: stockPurchase.total_costTotalOnUSD,
+          totalCostTotalOnUZSOnBuying: stockPurchase.total_costTotal,
           total_revenue:
             sale.total_income_amount -
             expense.total_expenses -
@@ -177,18 +326,23 @@ module.exports = {
       next(new ErrorHandler(400, "Failed to find orders", err.message));
     }
   },
-
   allYears: async function (req, res, next) {
     try {
       const { startYear, endYear } = req.body;
 
+      const startDate = moment(
+        `${startYear}-01-01-00:00`,
+        "YYYY-MM-DD-HH:mm"
+      ).toDate();
+      const endDate = moment(
+        `${endYear}-12-31-23:59`,
+        "YYYY-MM-DD-HH:mm"
+      ).toDate();
+
       const yearlySales = await SalesOrder.aggregate([
         {
           $match: {
-            date: {
-              $gte: `${startYear}-01-01-00:00`,
-              $lte: `${endYear}-12-31-23:59`,
-            },
+            date: { $gte: new Date(startDate), $lte: new Date(endDate) },
           },
         },
         {
@@ -213,10 +367,7 @@ module.exports = {
       const yearlyExpenses = await ExpenseSchema.aggregate([
         {
           $match: {
-            date: {
-              $gte: `${startYear}-01-01-00:00`,
-              $lte: `${endYear}-12-31-23:59`,
-            },
+            date: { $gte: new Date(startDate), $lte: new Date(endDate) },
           },
         },
         {
@@ -254,14 +405,61 @@ module.exports = {
         },
       ]);
 
-      const mergedResults = yearlySales.map((sale) => {
-        const year = sale._id.year;
+      const yearlyStockPurchases = await StockPurchase.aggregate([
+        {
+          $match: {
+            date: { $gte: new Date(startDate), $lte: new Date(endDate) },
+          },
+        },
+        {
+          $group: {
+            _id: { year: { $substr: ["$date", 0, 4] } },
+            total_shippingCost: { $sum: "$shippingCost" },
+            total_costTotalOnUSD: { $sum: "$costTotalOnUSD" },
+            total_costTotal: { $sum: "$costTotal" },
+          },
+        },
+        {
+          $sort: {
+            "_id.year": 1,
+          },
+        },
+      ]);
 
-        const expense = yearlyExpenses.find((exp) => exp._id.year === year) || {
+      const years = Array.from({ length: endYear - startYear + 1 }, (_, i) =>
+        (startYear + i).toString()
+      );
+
+      const mergedResults = years.map((year) => {
+        const sale = yearlySales.find((s) => s._id.year === year) || {
+          total_amount: 0,
+          total_origin_amount: 0,
+          total_income_amount: 0,
+          totalDebt: 0,
+          totalPaid: 0,
+          total_onUSD_amount: 0,
+          totalShippingCostOnSale: 0,
+          total_expenses: 0,
+          total_salaries: 0,
+          totalShippingCostOnBuying: 0,
+          totalCostTotalOnUSDOnBuying: 0,
+          totalCostTotalOnUZSOnBuying: 0,
+          total_revenue: 0,
+        };
+        const expense = yearlyExpenses.find((e) => e._id.year === year) || {
           total_expenses: 0,
         };
-        const salary = yearlySalaries.find((sal) => sal._id.year === year) || {
+        const salary = yearlySalaries.find(
+          (s) => s._id.year === parseInt(year, 10)
+        ) || {
           total_salaries: 0,
+        };
+        const stockPurchase = yearlyStockPurchases.find(
+          (sp) => sp._id.year === year
+        ) || {
+          total_shippingCost: 0,
+          total_costTotalOnUSD: 0,
+          total_costTotal: 0,
         };
 
         return {
@@ -269,12 +467,15 @@ module.exports = {
           total_amount: sale.total_amount,
           total_origin_amount: sale.total_origin_amount,
           total_income_amount: sale.total_income_amount,
+          total_onUSD_amount: sale.total_onUSD_amount,
           totalDebt: sale.totalDebt,
           totalPaid: sale.totalPaid,
-          total_onUSD_amount: sale.total_onUSD_amount,
-          shippingCost: sale.shippingCost,
+          totalShippingCostOnSale: sale.totalShippingCost,
           total_expenses: expense.total_expenses,
           total_salaries: salary.total_salaries,
+          totalShippingCostOnBuying: stockPurchase.total_shippingCost,
+          totalCostTotalOnUSDOnBuying: stockPurchase.total_costTotalOnUSD,
+          totalCostTotalOnUZSOnBuying: stockPurchase.total_costTotal,
           total_revenue:
             sale.total_income_amount -
             expense.total_expenses -
